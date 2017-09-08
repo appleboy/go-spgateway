d43052304771e914c801efb62b8ae9adad5a25bd
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-09-08 23:40:24 +0800

    add store struct.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- spgateway.go
+++ spgateway.go
@@ -7,8 +7,15 @@ import (
 	"strings"
 )
 
-// Handler provides spgateway config.
-type Handler struct {
+// Store provides spgateway config.
+type Config struct {
+	HashKey    string
+	HashIV     string
+	MerchantID string
+}
+
+// Store provides spgateway config.
+type Store struct {
 	HashKey    string
 	HashIV     string
 	MerchantID string
@@ -23,24 +30,24 @@ type Order struct {
 }
 
 // New returns a new empty handler.
-func New(MerchantID, HashKey, HashIV string) *Handler {
-	return &Handler{
-		MerchantID: MerchantID,
-		HashKey:    HashKey,
-		HashIV:     HashIV,
+func New(config Config) *Store {
+	return &Store{
+		MerchantID: config.MerchantID,
+		HashKey:    config.HashKey,
+		HashIV:     config.HashIV,
 	}
 }
 
 // CheckValue return spgateway check value for post data.
-func (h *Handler) CheckValue(order Order) string {
+func (s *Store) CheckValue(order Order) string {
 	querys := fmt.Sprintf("HashKey=%s&Amt=%s&MerchantID=%s&MerchantOrderNo=%s&TimeStamp=%s&Version=%s&HashIV=%s",
-		h.HashKey,
+		s.HashKey,
 		strconv.Itoa(order.Amt),
-		h.MerchantID,
+		s.MerchantID,
 		order.MerchantOrderNo,
 		order.TimeStamp,
 		order.Version,
-		h.HashIV,
+		s.HashIV,
 	)
 	hash := sha256.Sum256([]byte(querys))
 
--- spgateway_test.go
+++ spgateway_test.go
@@ -7,10 +7,11 @@ import (
 )
 
 func TestCheckValue(t *testing.T) {
-	MerchantID := "123456"
-	HashKey := "1A3S21DAS3D1AS65D1"
-	HashIV := "1AS56D1AS24D"
-	h := New(MerchantID, HashKey, HashIV)
+	store := New(Config{
+		MerchantID: "123456",
+		HashKey:    "1A3S21DAS3D1AS65D1",
+		HashIV:     "1AS56D1AS24D",
+	})
 
 	order := Order{
 		Amt:             200,
@@ -19,7 +20,7 @@ func TestCheckValue(t *testing.T) {
 		Version:         "1.1",
 	}
 
-	CheckValue := h.CheckValue(order)
+	CheckValue := store.CheckValue(order)
 	expect := "841F57D750FB4B04B62DDC3ECDC26F1F4028410927DD28BD5B2E34791CC434D2"
 
 	assert.Equal(t, expect, CheckValue)
